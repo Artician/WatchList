cc9b3cceea466030be32a0a2f77afd8d8032d92c
Jacob Moses <mosesjf17@gmail.com>
2021-08-04 04:06:11 -0400

    Initial commit, created the files, and will be adding more tomorrow.
    To do:
    Test connection to database
    Use data from database to populate recyclerview
    Use CardView to create a uniform look for movies
    add onClick listener to cardviews to show description

---
+++ app/src/main/java/com/example/watchlist/WebDisplayFragment.kt
@@ -0,0 +1,23 @@
+package com.example.watchlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+
+
+class WebDisplayFragment : Fragment() {
+    private val viewModel: MoviesViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_web_display, container, false)
+    }
+
+
+}
